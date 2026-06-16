49250a2e4e1fa9ddc69ac8c12dc31489ff2add5a
BastianReiter <150595257+BastianReiter@users.noreply.github.com>
2026-06-16 17:24:54 +0200

    Implemented additional table normalization method, more meta data

--- Development/Test/LocalTestTerminal.R
+++ Development/Test/LocalTestTerminal.R
@@ -7,7 +7,7 @@ library(dsFreda)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #RawDataSet <- readRDS(file = "./Development/Data/RealData/CCPRealData_Frankfurt.rds")
 #OldTestData <- readRDS(file = "./Development/Data/TestData/OldTestData/CCPTestData.rds")
-CCP.RawDataSet <- readRDS(file = "./Development/Data/TestData/CCPTestData.rds")
+#CCP.RawDataSet <- readRDS(file = "./Development/Data/TestData/CCPTestData.rds")
 #RawDataSet <- readRDS(file = "./Development/Data/TestData/CCPTestData_WithMissingTables.rds")
 
 
@@ -27,32 +27,32 @@ CCP.RawDataSet <- readRDS(file = "./Development/Data/TestData/CCPTestData.rds")
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Rename tables of RawDataSet (the names are also changed when tables are being loaded into R server sessions)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-vc_Lookup <- dsCCPhos::Meta.Tables$TableName.Curated
-names(vc_Lookup) <- dsCCPhos::Meta.Tables$TableName.Raw
-
-names(CCP.RawDataSet) <- sapply(names(CCP.RawDataSet),
-                            function(TableName) { vc_Lookup[TableName] })
+# vc_Lookup <- dsCCPhos::Meta.Tables$TableName.Curated
+# names(vc_Lookup) <- dsCCPhos::Meta.Tables$TableName.Raw
+#
+# names(CCP.RawDataSet) <- sapply(names(CCP.RawDataSet),
+#                             function(TableName) { vc_Lookup[TableName] })
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Perform preparatory operations prior to curation
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-RDSPreparation <- PrepareRawDataDS(RawDataSetName.S = "CCP.RawDataSet",
-                                   Module.S = "CCP",
-                                   RunFuzzyStringMatching.S = FALSE,
-                                   CurateFeatureNames.S = TRUE)
-
-CCP.RawDataSet <- RDSPreparation$RawDataSet
+# RDSPreparation <- PrepareRawDataDS(RawDataSetName.S = "CCP.RawDataSet",
+#                                    Module.S = "CCP",
+#                                    RunFuzzyStringMatching.S = FALSE,
+#                                    CurateFeatureNames.S = TRUE)
+#
+# CCP.RawDataSet <- RDSPreparation$RawDataSet
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Check Tables for existence and completeness
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-RDSTableCheck <- dsFreda::GetDataSetCheckDS(DataSetName.S = "CCP.RawDataSet",
-                                            Module.S = "CCP",
-                                            Stage.S = "Raw")
+# RDSTableCheck <- dsFreda::GetDataSetCheckDS(DataSetName.S = "CCP.RawDataSet",
+#                                             Module.S = "CCP",
+#                                             Stage.S = "Raw")
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -73,25 +73,20 @@ RDSTableCheck <- dsFreda::GetDataSetCheckDS(DataSetName.S = "CCP.RawDataSet",
 #                                SampleSize.S = 1000)
 
 
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Load CCP test data as raw data set
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+CCP.RawDataSet <- readRDS("../Data/CCP/CCPTestData2026.rds")
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Curate data
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 CurationOutput <- dsFreda::CurateDataDS(RawDataSetName.S = "CCP.RawDataSet",
                                         Module.S = "CCP")
 
-
-# CurationOutput$CurationReport$EntryCounts
-# View(CurationOutput$CurationReport$Transformation$Monitors$RadiationTherapy)
-# View(CurationOutput$CurationReport$Transformation$Monitors$Staging)
-# View(CurationOutput$CurationReport$Transformation$EligibilityOverviews$Staging)
-# View(CurationOutput$CurationReport$Transformation$ValueSetOverviews$Staging$Harmonized)
-
-
 CCP.CuratedDataSet <- CurationOutput$DataSet
-
-CDSTableCheck <- dsFreda::GetDataSetCheckDS(DataSetName.S = "CCP.CuratedDataSet",
-                                            Module.S = "CCP",
-                                            Stage.S = "Curated")
 
 
 
